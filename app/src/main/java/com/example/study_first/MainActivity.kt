d4840d1aba05ca68b8e2ac2f1f2096f980631904
wand96 <tjsrb96@naver.com>
2021-10-13 20:56:15 +0900

    [feature] when 사용하기.

--- app/src/main/java/com/example/study_first/MainActivity.kt
+++ app/src/main/java/com/example/study_first/MainActivity.kt
@@ -9,9 +9,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var a = 5
-        var b = 3
-        var bigger = if(a > b) a else b
-
+        var now = 10
+        when (now) {
+            8 -> {
+                Log.d("when", "현재 시간은 8시입니다.")
+            }
+            9 -> {
+                Log.d("when", "현재 시간은 9시입니다.")
+            }
+            else -> {
+                Log.d("when", "현재 시간은 9시가 아닙니다.")
+            }
+        }
     }
 }
