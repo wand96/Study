9af684ca0a8f11243b2f97de97d96d0c627b9d7f
wand96 <tjsrb96@naver.com>
2021-10-13 20:49:15 +0900

    [feature] 변수에 직접 if문 사용해보기.

--- app/src/main/java/com/example/study_first/MainActivity.kt
+++ app/src/main/java/com/example/study_first/MainActivity.kt
@@ -9,12 +9,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var ball = 4
-        if(ball > 3) {
-            Log.d("ControlFlow", "4볼로 출루합니다.")
-        } else {
-            Log.d("ControlFlow", "타석에서 다음 타구를 기다립니다.")
-        }
+        var a = 5
+        var b = 3
+        var bigger = if(a > b) a else b
 
     }
 }
