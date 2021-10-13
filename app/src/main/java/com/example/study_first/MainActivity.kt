214aaf3256beb11b6ee42c3df0c06d4ff42628e1
wand96 <tjsrb96@naver.com>
2021-10-13 20:43:29 +0900

    [feature] if문 사용해보기.

--- app/src/main/java/com/example/study_first/MainActivity.kt
+++ app/src/main/java/com/example/study_first/MainActivity.kt
@@ -9,12 +9,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var myName = "김선규"
-        var myAge: Int
-        myAge = 27
-        myAge = myAge + 1
-        Log.d("BasicSyntax", "myName = $myName, myAge = $myAge")
+        var myNumbers = "1, 2, 3, 4, 5, 6"
+        var thisWeekNumbers = "5, 6, 7, 8, 9, 10"
 
+        if(myNumbers == thisWeekNumbers) {
+            Log.d("Lotto", "당첨되었습니다.")
+        }
 
     }
 }
