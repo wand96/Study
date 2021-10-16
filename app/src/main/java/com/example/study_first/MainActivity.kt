4bf541ff0ec7e1c1a0607162661bfcbe7fa6d518
wand96 <tjsrb96@naver.com>
2021-10-16 11:57:33 +0900

    [feature] 배열 사용하기.

--- app/src/main/java/com/example/study_first/MainActivity.kt
+++ app/src/main/java/com/example/study_first/MainActivity.kt
@@ -9,17 +9,35 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var now = 10
-        when (now) {
-            8 -> {
-                Log.d("when", "현재 시간은 8시입니다.")
-            }
-            9 -> {
-                Log.d("when", "현재 시간은 9시입니다.")
-            }
-            else -> {
-                Log.d("when", "현재 시간은 9시가 아닙니다.")
-            }
-        }
+        var students = IntArray(10)
+        var longArray = LongArray(10)
+        var CharArray = CharArray(10)
+        var FloatArray = FloatArray(10)
+        var DoubleArray = DoubleArray(10)
+
+        var intArray = intArrayOf(1,2,3,4,5,6,7,8,9,10)
+
+        var stringArray = Array(10, {item->""})
+        var dayArray = arrayOf("MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN")
+
+        students[0] = 90
+        students[1] = 91
+        students[2] = 92
+        students[3] = 93
+        students[4] = 94
+
+        students.set(5, 95)
+        students.set(6, 96)
+        students.set(7, 97)
+        students.set(8, 98)
+        students.set(9, 99)
+
+        intArray[6] = 137
+        intArray.set(9, 200)
+
+        var seventhValue = intArray[6]
+        Log.d("Array", "여덟 번째 intARray의 값은 ${seventhValue}입니다.")
+        var tenthValue = intArray.get(9)
+        Log.d("Array", "열 번째 intARray의 값은 ${tenthValue}입니다.")
+
     }
-}
