64f36b6ce3f2365f66eab716dbb8cd3d23dc26c0
wand96 <tjsrb96@naver.com>
2021-10-17 16:19:57 +0900

    [feature] Collection프로젝트: 리스트 다루기

--- app/src/main/java/com/example/study_first/MainActivity.kt
+++ app/src/main/java/com/example/study_first/MainActivity.kt
@@ -9,37 +9,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //1. 값으로 컬렉션 생성하기
         var mutableList = mutableListOf("MON", "TUE", "WED")
-
-        //동적으로 리스트의 공간 증가
+        // 값을 추가합니다.
         mutableList.add("THU")
+        // 값을 꺼냅니다.
+        Log.d("Collection", "mutableList의 첫 번째 값은 ${mutableList.get(0)}입니다.")
+        Log.d("Collection", "mutableList의 두 번째 값은 ${mutableList.get(1)}입니다.")
 
-        //get: 리스트에 입력된 값 사용하기
-        var variable = mutableList.get(1)
-
-        //set: 리스트값 수정하기
-        mutableList.set(1, "수정할 값")
-
-        //removeAt
-        mutableList.removeAt(1)
-
-        //빈 리스트 사용하기
-        //빈 리스트 사용하기 - 생성
+        //2. 빈 컬렉션 생성하기
         var stringList = mutableListOf<String>()
-
-        //빈 리스트 사용하기 - 입력
+        //값을 추가합니다.
         stringList.add("월")
         stringList.add("화")
-
-        //빈 리스트 사용하기 - 사용
+        stringList.add("수")
+        //값을 변경합니다
+        stringList.set(1, "요일 변경")
+        // 사용
         Log.d("Collection", "stringList에 입력된 두 번째 값은 ${stringList.get(1)}입니다.")
-
-        //빈 리스트 사용하기 - 수정
-        stringList.set(1, "수정된 값")
-
-        //빈 리스트 사용하기 - 삭제
+        //삭제
         stringList.removeAt(1)
+        Log.d("Collection", "stringList에 입력된 두 번째 값은 ${stringList.get(1)}입니다.")
+        //개수를 출력합니다.
+        Log.d("Collection", "stringList에는 ${stringList.size}개의 값이 있습니다.")
 
-       }
-
+    }
 }
