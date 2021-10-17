c420a14967928267d58a0d6287bccdd5b19db585
wand96 <tjsrb96@naver.com>
2021-10-17 16:10:49 +0900

    [feature] list 사용하기.

--- app/src/main/java/com/example/study_first/MainActivity.kt
+++ app/src/main/java/com/example/study_first/MainActivity.kt
@@ -9,46 +9,37 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //1. 기본 타입 배열 선언하기 - 각 기본 타입별로 10개의 빈 공간이 할당됩니다.
-        var students = IntArray(10)
-        var longArray = LongArray(10)
-        var CharArray = CharArray(10)
-        var FloatArray = FloatArray(10)
-        var DoubleArray = DoubleArray(10)
-        //arrayOf 함수를  사용하면 선언과 동시에 값을 입력할 수 있습니다.
-        var intArray = intArrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+        var mutableList = mutableListOf("MON", "TUE", "WED")
 
-        //2.문자열 타입 배열 선언하기
-        var stringArray = Array(10, { item -> "" })
-        //arrayOf 함수로 값을 직접 입력해서 배열을 생성할 수 있습니다.
-        var dayArray = arrayOf("MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN")
+        //동적으로 리스트의 공간 증가
+        mutableList.add("THU")
 
-        //3. 앞에서 선언한 students 변수에 값 넣기
-        //가. 대괄호를 사용하는 방법
-        students[0] = 90
-        students[1] = 91
-        students[2] = 92
-        students[3] = 93
-        students[4] = 94
-        //나. set 함수를 사용하는 방법
-        students.set(5, 95)
-        students.set(6, 96)
-        students.set(7, 97)
-        students.set(8, 98)
-        students.set(9, 99)
+        //get: 리스트에 입력된 값 사용하기
+        var variable = mutableList.get(1)
 
-        //4. 값 변경해보기
-        intArray[6] = 137
-        intArray.set(9, 200)
+        //set: 리스트값 수정하기
+        mutableList.set(1, "수정할 값")
 
-        //5. 배열 값 변경해보기
-        var seventhValue = intArray[6]
-        Log.d("Array", "여덟 번째 intArray의 값은 ${seventhValue}입니다.")
-        var tenthValue = intArray.get(9)
-        Log.d("Array", "열 번째 intArray의 값은 ${tenthValue}입니다.")
+        //removeAt
+        mutableList.removeAt(1)
 
-        //6. 변수에 담지 않고 직접 사용해도 된다.
-        Log.d("Array", "첫 번째 dayArray의 값은 ${dayArray[0]}입니다.")
-        Log.d("Array", "여섯 번째 dayArray의 값은 ${dayArray.get(5)}입니다.")
-    }
+        //빈 리스트 사용하기
+        //빈 리스트 사용하기 - 생성
+        var stringList = mutableListOf<String>()
+
+        //빈 리스트 사용하기 - 입력
+        stringList.add("월")
+        stringList.add("화")
+
+        //빈 리스트 사용하기 - 사용
+        Log.d("Collection", "stringList에 입력된 두 번째 값은 ${stringList.get(1)}입니다.")
+
+        //빈 리스트 사용하기 - 수정
+        stringList.set(1, "수정된 값")
+
+        //빈 리스트 사용하기 - 삭제
+        stringList.removeAt(1)
+
+       }
+
 }
