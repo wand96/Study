0dea5930fa16f30f803e9a9a9a0d3e14a09eeecf
wand96 <tjsrb96@naver.com>
2021-10-18 22:30:35 +0900

    [feature] Collection프로젝트: 맵 다루기

--- app/src/main/java/com/example/study_first/MainActivity.kt
+++ app/src/main/java/com/example/study_first/MainActivity.kt
@@ -9,18 +9,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //1. 셋 생성하고 값 추가하기
-        var set = mutableSetOf<String>()
-        set.add("JAN")
-        set.add("FEB")
-        set.add("MAR")
-        set.add("JAN")
-
-        //2. 전체 데이터 출력해보기
-        Log.d("Collection", "Set 전체 출력 = ${set}")
-
-        //3. 특정 값 삭제하기
-        set.remove("FEB")
-        Log.d("Collection", "Set 전체 출력 = ${set}")
+        //1. 맵 생성하기
+        var map = mutableMapOf<String, String>()
+        //2. 값 넣기
+        map.put("키1", "값2")
+        map.put("키2", "값2")
+        map.put("키3", "값3")
+        //3. 값 사용하기
+        var variable = map.get("키2")
+        Log.d("Collection", "키2의 값은 ${variable}입니다.")
+        //4.값 수정하기
+        map.put("키2", "두 번째 값 수정")
+        Log.d("Collection", "키2의 값은 ${map.get("키2")}입니다.")
+        //5. 값 삭제하기
+        map.remove("키2")
+        //5.1 없는 값을 불러오면 null 값이 출력된다.
+        Log.d("Collection", "키2의 값은 ${map.get("키2")}입니다.")
     }
 }
