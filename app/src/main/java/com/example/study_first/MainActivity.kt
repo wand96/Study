9eade57ed3b128ee58b15965908cdca0dc83e4c4
wand96 <tjsrb96@naver.com>
2021-10-19 19:29:34 +0900

    [feature] ControlFlowWhile: while과 반복문 제어하기

--- app/src/main/java/com/example/study_first/MainActivity.kt
+++ app/src/main/java/com/example/study_first/MainActivity.kt
@@ -3,38 +3,59 @@ package com.example.study_first
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.core.util.rangeTo
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //1. 일반적인 반복문 사용으로 열 번 반복하기
-        for (index in 1..10){
-            Log.d("For", "현재 숫자는 ${index}")
+        //1.일반적인 while 사용하기
+        var current = 1
+        val until = 12
+        while (current < until) {
+            Log.d("while", "현재 값은 ${current}입니다.")
+            //current를 1씩 증가시켜서 열한 번 반복한 후 while 문을 빠져나갑니다.
+            current = current + 1
         }
 
-        //2. 마지막 숫자 제외하기
-        var array = arrayOf("JAN", "FEB", "MAR", "APR", "MAY", "JUN")
-        for (index in 0 until array.size) {
-            Log.d("For", "현재 월은 ${array.get(index)}입니다.")
+        //2. do ~ while 사용하기
+        var game = 1
+        val match = 6
+        do {
+            Log.d("while", "${game}게임 이겼습니다. 우승까지 ${match-game}게임 남았습니다.")
+            game += 1
+        }while (game < match)
+
+        //3. while vs do ~ while
+        //while 테스트
+        game = 6
+        while(game < match) {
+            Log.d("while", "****while 테스트입니다.****")
+            game += 1
         }
-        //3. 건너뛰기
-        for (index in 0..10 step 3) {
-            Log.d("For", "건너뛰기: ${index}")
+
+        //do ~ while 테스트
+        game = 6
+        do {
+            Log.d("while", "****do ~ while 테스트입니다.****")
+            game += 1
+        } while (game < match)
+
+        //4. break 반복문 탈출하기
+        for (index in 1..10) {
+            Log.d("while", "break > 현재 index는 $index 입니다.")
+            if (index > 5) {
+                break
+            }
         }
-        //4. 감소시키기
-        for (index in 10 downTo 0) {
-            Log.d("For", "감소시키기: ${index}")
-        }
-        //4.1 건너뛰면서 감소시키기
-        for (index in 10 downTo 0 step 3) {
-            Log.d("For", "건너뛰면서 감소시키기: ${index}")
-        }
-        //5.1 배열, 컬렉션 사용하기
-        for (month in array) {
-            Log.d("For", "현재 월은 ${month}입니다.")
+
+        //5. continue 다음 반복문으로
+        for (except in 1..10) {
+            if (except in 1..10) {
+                continue
+            }
+            Log.d("while", "continue > 현재 index는 $except 입니다.")
         }
     }
-
 }
