84248452f35bc6c1f38c82ee5a7e784328cab401
wand96 <tjsrb96@naver.com>
2021-10-20 10:51:00 +0900

    [feature] Function 프로젝트: 파라미터의 기본값 정의와 호출

--- app/src/main/java/com/example/study_first/MainActivity.kt
+++ app/src/main/java/com/example/study_first/MainActivity.kt
@@ -10,31 +10,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //반환값과 입력값이 있는 함수의 정의
-        fun square(x: Int): Int {
-            return x * x
+        fun newFunction(name: String, age: Int = 29, weight:Double = 65.5) {
+            Log.d("fun", "name의 값은 ${name}입니다.")
+            Log.d("fun", "age의 값은 ${age}입니다.")
+            Log.d("fun", "weight의 값은 ${weight}입니다.")
         }
 
-        //반환값이 없는 함수의 정의
-        fun printSum(x: Int, y: Int) {
-            Log.d("fun", "x + y = ${x+y}")
-        }
-
-        //입력값 없이 반환값만 있는 함수의 정의
-        fun getPi(): Double {
-            return 3.14
-        }
-
-        //반환값과 입력값이 있는 함수의 호출
-        var squareResult = square(30)
-        Log.d("fun", "30의 제곱은 ${squareResult}입니다.")
-
-        //반환값이 없는 함수의 호출
-        printSum(3, 5)
-
-        //입력값이 없는 함수의 호출
-        val PI = getPi()
-        Log.d("fun", "지름이 10인 원의 둘레는 ${10 * PI}입니다.")
-
+        newFunction("Hello")
     }
 }
