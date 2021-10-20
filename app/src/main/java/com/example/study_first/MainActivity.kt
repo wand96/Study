0ec35cf1dfacd027e5e2a042daf91ceb4c04acdb
wand96 <tjsrb96@naver.com>
2021-10-20 17:53:10 +0900

    [feature] DesignTool 프로젝트: 설계 도구 이해하기

--- app/src/main/java/com/example/study_first/MainActivity.kt
+++ app/src/main/java/com/example/study_first/MainActivity.kt
@@ -10,18 +10,59 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //추상화: walk는 걷다가 맞지만 move는 상속받을 자식 클래스의 속성에 따라 바뀜
-        abstract class Animal {
-            fun walk() {
-                Log.d("abstract", "걷습니다.")
-            }
-            abstract fun move()
-        }
+        //접근 제한자 테스트
+        var child = Child()
+        child.callVariables()
 
-        class Bird: Animal() {
-            override fun move() {
-                Log.d("abstract", "날아서 이동합니다.")
-            }
-        }
+        //부모 클래스 직접 호출해보기
+        var parent = Parent()
+        Log.d("Visibility", "Parent: 기본 제한자 defaultVal의 값은 ${parent.defaultVal}")
+        Log.d("Visibility", "internalVal의 값은 ${parent.internalVal}")
+    }
+}
+
+//추상 클래스 설계
+abstract class Animal {
+    fun walk() {
+        Log.d("abstract", "걷습니다.")
+    }
+    abstract fun move()
+}
+//구현
+class Bird: Animal() {
+    override fun move() {
+        Log.d("abstract", "날아서 이동합니다.")
+    }
+}
+//인터페이스 설계
+interface InterfaceKotlin {
+    var variable: String
+    fun get()
+    fun set()
+}
+//구현
+class KotlinImpl: InterfaceKotlin {
+    override var variable: String = "init value"
+    override fun get() {
+        //코드 구현
+    }
+    override fun set() {
+        //코드 구현
+    }
+}
+//접근 제한자 테스트를 위한 부모 클래스
+open class Parent {
+    private val priateVal = 1
+    protected open val protectedVal = 2
+    internal val internalVal = 3
+    val defaultVal = 4
+}
+//자식 클래스
+class Child: Parent() {
+    fun callVariables() {
+        //privateVal은 호출이 안됩니다.
+        Log.d("Visibility", "Child: protectedval의 값은 ${protectedVal}")
+        Log.d("Visibility", "Child: internalVal의 값은 ${internalVal}")
+        Log.d("Visibility", "Child: 기본 제한자 defaultVal의 값은 ${defaultVal}")
     }
 }
