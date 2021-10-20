186b61e1715f6a1dd9ce723845eef71480ede7dd
wand96 <tjsrb96@naver.com>
2021-10-20 10:34:58 +0900

    [feature] 반환값과 입력값이 있는 함수의 정의

--- app/src/main/java/com/example/study_first/MainActivity.kt
+++ app/src/main/java/com/example/study_first/MainActivity.kt
@@ -10,52 +10,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //1.일반적인 while 사용하기
-        var current = 1
-        val until = 12
-        while (current < until) {
-            Log.d("while", "현재 값은 ${current}입니다.")
-            //current를 1씩 증가시켜서 열한 번 반복한 후 while 문을 빠져나갑니다.
-            current = current + 1
-        }
-
-        //2. do ~ while 사용하기
-        var game = 1
-        val match = 6
-        do {
-            Log.d("while", "${game}게임 이겼습니다. 우승까지 ${match-game}게임 남았습니다.")
-            game += 1
-        }while (game < match)
-
-        //3. while vs do ~ while
-        //while 테스트
-        game = 6
-        while(game < match) {
-            Log.d("while", "****while 테스트입니다.****")
-            game += 1
-        }
-
-        //do ~ while 테스트
-        game = 6
-        do {
-            Log.d("while", "****do ~ while 테스트입니다.****")
-            game += 1
-        } while (game < match)
-
-        //4. break 반복문 탈출하기
-        for (index in 1..10) {
-            Log.d("while", "break > 현재 index는 $index 입니다.")
-            if (index > 5) {
-                break
-            }
-        }
-
-        //5. continue 다음 반복문으로
-        for (except in 1..10) {
-            if (except in 1..10) {
-                continue
-            }
-            Log.d("while", "continue > 현재 index는 $except 입니다.")
+        //반환값과 입력값이 있는 함수의 정의
+        fun square(x: Int): Int {
+            return x * x
         }
     }
 }
