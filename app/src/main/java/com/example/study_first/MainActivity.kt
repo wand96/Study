00ef71e9a68cd3ecd0391bba78c48c3b2f74e1f1
wand96 <tjsrb96@naver.com>
2021-10-21 16:18:48 +0900

    [feature] null 값으로 인한 오류 상황.

--- app/src/main/java/com/example/study_first/MainActivity.kt
+++ app/src/main/java/com/example/study_first/MainActivity.kt
@@ -9,60 +9,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        //접근 제한자 테스트
-        var child = Child()
-        child.callVariables()
-
-        //부모 클래스 직접 호출해보기
-        var parent = Parent()
-        Log.d("Visibility", "Parent: 기본 제한자 defaultVal의 값은 ${parent.defaultVal}")
-        Log.d("Visibility", "internalVal의 값은 ${parent.internalVal}")
+        
+        //오류가 날 수 밖에 없는 상황
+        //one이 One의 형식으로 생성되었지만 생성자를 호출해서 저장되지는 않음
+        //1>2가 false라서 비어있는 상태로 one변수는 null임 
+        var one: One
+        if (1 > 2) {
+            one = One()
+        }
+        one.print()
     }
 }
 
-//추상 클래스 설계
-abstract class Animal {
-    fun walk() {
-        Log.d("abstract", "걷습니다.")
-    }
-    abstract fun move()
-}
-//구현
-class Bird: Animal() {
-    override fun move() {
-        Log.d("abstract", "날아서 이동합니다.")
-    }
-}
-//인터페이스 설계
-interface InterfaceKotlin {
-    var variable: String
-    fun get()
-    fun set()
-}
-//구현
-class KotlinImpl: InterfaceKotlin {
-    override var variable: String = "init value"
-    override fun get() {
-        //코드 구현
-    }
-    override fun set() {
-        //코드 구현
-    }
-}
-//접근 제한자 테스트를 위한 부모 클래스
-open class Parent {
-    private val priateVal = 1
-    protected open val protectedVal = 2
-    internal val internalVal = 3
-    val defaultVal = 4
-}
-//자식 클래스
-class Child: Parent() {
-    fun callVariables() {
-        //privateVal은 호출이 안됩니다.
-        Log.d("Visibility", "Child: protectedval의 값은 ${protectedVal}")
-        Log.d("Visibility", "Child: internalVal의 값은 ${internalVal}")
-        Log.d("Visibility", "Child: 기본 제한자 defaultVal의 값은 ${defaultVal}")
+class One {
+    fun print() {
+        Log.d("null_safety", "can you call me?")
     }
 }
