3dc131db6d87731bb2dbb2fb525df2e3308d417a
wand96 <tjsrb96@naver.com>
2021-11-04 17:24:31 +0900

    [feature] 마지막 실행 코드를 반환하는 스코프 함수: let, run, with

--- app/src/main/java/com/example/study_first/MainActivity.kt
+++ app/src/main/java/com/example/study_first/MainActivity.kt
@@ -12,19 +12,25 @@ class MainActivity : AppCompatActivity() {
 
 
         //스코프함수
-        //호출 대상인 this 자체를 반환하는 스코프 함수: apply, also
+        //마지막 실행 코드를 반환하는 스코프 함수: let, run, with
         var list = mutableListOf("Scope", "Function")
 
-        val afterApply = list.apply {
-            add("Apply")
-            count()
-        }
-        println("반환값 apply = $afterApply")
-
-        val afterAlso = list.also {
-            it.add("Also")
+        val lastCount = list.let {
+            it.add("Run")
             it.count()
         }
-        println("반환값 also = $afterAlso")
+        println("반환값 let = $lastCount")
+
+        val lastItem = list.run {
+            add("Run")
+            get(size-1)
+        }
+        println("반환값 run = $lastItem")
+
+        val lastItemWith = with(list) {
+            add("with")
+            get(size-1)
+        }
+        println("반환값 with = $lastItemWith")
     }
 }
