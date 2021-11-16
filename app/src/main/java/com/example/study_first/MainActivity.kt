61bde24f3496812091aad397ef0848b2a0fec2f8
wand96 <tjsrb96@naver.com>
2021-11-16 11:53:22 +0900

    [feature] 시크바

--- app/src/main/java/com/example/study_first/MainActivity.kt
+++ app/src/main/java/com/example/study_first/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.CompoundButton
+import android.widget.SeekBar
 import androidx.core.util.rangeTo
 import androidx.core.widget.addTextChangedListener
 import com.example.study_first.databinding.ActivityMainBinding
@@ -18,24 +19,21 @@ import kotlin.concurrent.thread
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        thread(start=true) {        //서브스레드
-            Thread.sleep(3000)
-            runOnUiThread {
-                showProgress(false)     //메인스레드
+        binding.seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                binding.textView.text = "$progress"
             }
-        }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+                //TODO("Not yet implemented")
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+                //TODO("Not yet implemented")
+            }
+
+        })
+
     }
 
-    fun showProgress(show: Boolean) {
-        binding.progressLayout.visibility = if(show) View.VISIBLE else View.GONE
-//
-//        if(show) {
-//            binding.progressLayout.visibility = View.VISIBLE
-//        } else {
-//            binding.progressLayout.visibility = View.GONE
-//        }
-
-    }
-}
-  //thread() 함수는 서브스레드임 -> 그래서 서브스레드에서 3초가 멈춘 후에
-  // runOnUiThread를 이용 메인 메서드에서 showProgress를 이용 showProgress함수를 false로 해주면 메인 은 살아있음
+  }
