aa93a37d90735db976a720be1e9f2835c7237b22
wand96 <tjsrb96@naver.com>
2021-11-17 20:18:02 +0900

    액티비티 사이에 값 주고받기

--- app/src/main/java/com/example/study_first/MainActivity.kt
+++ app/src/main/java/com/example/study_first/MainActivity.kt
@@ -7,6 +7,7 @@ import android.util.Log
 import android.view.View
 import android.widget.CompoundButton
 import android.widget.SeekBar
+import android.widget.Toast
 import androidx.core.util.rangeTo
 import androidx.core.widget.addTextChangedListener
 import com.example.study_first.databinding.ActivityMainBinding
@@ -24,8 +25,22 @@ import kotlin.concurrent.thread
         intent.putExtra("from1", "Hello Bundle")
         intent.putExtra("from2", 2021)
 
-        binding.btnStart.setOnClickListener { startActivity(intent) }
+        binding.btnStart.setOnClickListener { startActivityForResult(intent, 99) }
+//        메인 액티비티에서 서브 액티비티를 호출한 후 값을 돌려받고 싶을 때는 startActivity()가 아니라 startActivityForResult()메서드를 사용해야한다
+//        startActivityForResult()메서드의 두번째 파라미터는 서브 액티비티를 호출하는 버튼이 여러 개 있을 때 어떤 버튼에서 호출된 거인지를 구분하는 용도
 
     }
 
+      override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+          super.onActivityResult(requestCode, resultCode, data)
+
+          if (resultCode == RESULT_OK) {
+              when (requestCode) {
+                  99 -> {
+                      val message = data?.getStringExtra("returnValue")
+                      Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+                  }
+              }
+          }
+      }
   }
--- app/src/main/java/com/example/study_first/SubActivity.kt
+++ app/src/main/java/com/example/study_first/SubActivity.kt
@@ -1,5 +1,6 @@
 package com.example.study_first
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.study_first.databinding.ActivitySubBinding
@@ -14,5 +15,12 @@ class SubActivity : AppCompatActivity() {
 
         binding.to1.text = intent.getStringExtra("from1")
         binding.to2.text = "${intent.getIntExtra("from2", 0)}"
+
+        binding.btnClose.setOnClickListener {
+            val returnIntent = Intent()
+            returnIntent.putExtra("returnValue", binding.editMessage.text.toString())
+            setResult(RESULT_OK, returnIntent)
+            finish()
+        }
     }
 }
